161967669dc6fcd5bf1e1cccaec48a6a9b432229
JeongSeonggil <dataofsg02@gmail.com>
2022-04-11 15:17:39 +0900

    Initial commit

---
+++ UserService/src/main/java/com/submarket/userservice/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.submarket.userservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class MainController {
+    private Environment env;
+
+    @Autowired
+    public MainController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        log.info("UserService On");
+        return env.getProperty("spring.application.name")
+                + ", port(local.server.port) : " + env.getProperty("local.server.port")
+                + ", port(server.port) : " + env.getProperty("server.port")
+                + ", token secret : " + env.getProperty("token.secret")
+                + ", token expiration time : " + env.getProperty("token.expiration_time");
+    }
+}
