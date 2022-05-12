f64cc38ecdcfd93575c020430cb6168348584e8f
JeongSeonggil <dataofsg02@gmail.com>
2022-05-12 19:55:35 +0900

    todo Gateway

--- ApiGateway/src/main/java/com/submarket/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/submarket/apigateway/ApiGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
+    // TODO: 2022/05/12 Application.yml 회원가입 토큰 검사 수 ( 회원가입은 토큰 검증 X )
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
