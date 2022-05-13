571cda51285acbebd30ffec39d666c43d3af5acf
JeongSeonggil <dataofsg02@gmail.com>
2022-05-13 23:28:02 +0900

    JPA 수정

--- ItemService/src/main/java/com/submarket/itemservice/controller/MainController.java
+++ ItemService/src/main/java/com/submarket/itemservice/controller/MainController.java
@@ -1,10 +1,17 @@
 package com.submarket.itemservice.controller;
 
+import com.submarket.itemservice.dto.CategoryDto;
+import com.submarket.itemservice.jpa.CategoryRepository;
+import com.submarket.itemservice.jpa.entity.CategoryEntity;
+import com.submarket.itemservice.mapper.CategoryMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     private final Environment env;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping("/health")
     public String health() {
@@ -21,5 +29,16 @@ public class MainController {
                 + ", port(server.port) : " + env.getProperty("server.port")
                 + ", token secret : " + env.getProperty("token.secret")
                 + ", token expiration time : " + env.getProperty("token.expiration_time");
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<CategoryEntity> test() throws Exception {
+
+        Optional<CategoryEntity> categoryEntity = categoryRepository.findById(1);
+
+        CategoryEntity categoryEntity1 = categoryEntity.get();
+
+
+        return ResponseEntity.ok().body(categoryEntity1);
     }
 }
--- ItemService/src/main/java/com/submarket/itemservice/jpa/entity/CategoryEntity.java
+++ ItemService/src/main/java/com/submarket/itemservice/jpa/entity/CategoryEntity.java
@@ -1,5 +1,6 @@
 package com.submarket.itemservice.jpa.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
