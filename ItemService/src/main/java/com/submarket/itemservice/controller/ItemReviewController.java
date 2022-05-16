d09055c1c7fabe0b36c92d12801d62ced974a5ca
JeongSeonggil <dataofsg02@gmail.com>
2022-05-16 11:20:32 +0900

    리뷰 조회

--- ItemService/src/main/java/com/submarket/itemservice/controller/ItemReviewController.java
+++ ItemService/src/main/java/com/submarket/itemservice/controller/ItemReviewController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Path;
+import java.util.List;
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -44,6 +47,17 @@ public class ItemReviewController {
         log.info(this.getClass().getName() + ".modifyItemReview End!");
 
         return ResponseEntity.status(HttpStatus.OK).body("리뷰 변경 완료");
+    }
+
+    @GetMapping("/item/{itemSeq}/review")
+    public ResponseEntity<List<ItemReviewDto>> findItemReviewInItem(@PathVariable int itemSeq) throws Exception {
+        log.info(this.getClass().getName() + "findItemReviewInItem Start!");
+
+        List<ItemReviewDto> itemReviewDtoList = itemReviewService.findAllReviewInItem(itemSeq);
+
+        log.info(this.getClass().getName() + "findItemReviewInItem End!");
+
+        return ResponseEntity.ok().body(itemReviewDtoList);
     }
 
 }
--- ItemService/src/main/java/com/submarket/itemservice/service/IItemReviewService.java
+++ ItemService/src/main/java/com/submarket/itemservice/service/IItemReviewService.java
@@ -2,8 +2,12 @@ package com.submarket.itemservice.service;
 
 import com.submarket.itemservice.dto.ItemReviewDto;
 
+import java.util.List;
+
 public interface IItemReviewService {
     int saveReview(ItemReviewDto itemReviewDto, int itemSeq) throws Exception;
 
     int modifyReview(ItemReviewDto itemReviewDto) throws Exception;
+
+    List<ItemReviewDto> findAllReviewInItem(int itemSeq) throws Exception;
 }
